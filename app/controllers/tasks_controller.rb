77c6958e71f9f36e61093d7e720e0e8755da7ba3
Fumihisa Suzuki <fumi_s_qrio192@yahoo.co.jp>
2019-11-21 22:36:46 +0000

    C5 Create index of Tasks(not short time)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+    def index
+        @tasks=Task.where(user_id: params[:user_id])
+    end
+    def create
+    end
+    
+  private
+    # 1ヶ月分の勤怠情報を扱います。
+    def tasks_params
+      params.require(:user).permit(tasks: [:name, :note, :created_at])[:tasks]
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,10 @@ User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+              
+
+
+# タスクをひとつずつ作成します。
+Task.create!(name: 'タイピングゲーム', note: '朝10分のタイピングゲームを行う。', user_id: '1')
+Task.create!(name: 'PDCA報告', note: '朝に書いてしまう。', user_id: '1')
+Task.create!(name: '瞑想', note: '出来れば', user_id: '1')
